277711e07c3c178707c71bb7f47900c37682afd7
Sernny1369 <135222155+Sernny1369@users.noreply.github.com>
2025-04-07 11:25:28 +0200

    Create server.js

---
+++ back/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Route test
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello depuis le backend !" });
+});
+
+// Route test
+app.get("/api/planning", (req, res) => {
+  res.json({ message: "voici ton planing" });
+});
+
+app.post("/api/planning", (req, res) => {
+  const { planning } = req.body;
+  if (!planning) {
+    return res.status(400).json({ error: "Planning data is required" });
+  }
+  res.status(201).json({ message: "Planning received", planning });
+}
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur en ligne sur http://localhost:${PORT}`);
+});
